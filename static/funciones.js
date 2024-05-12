371c9584c76ffc52366af6fefb40d6c3ad6f15b9
Majo <descalsmaria@hotmail.com>
2024-05-12 16:07:26 -0300

    se suben cambios pagina de inicio y preguntas frecuentes css

---
+++ static/funciones.js
@@ -0,0 +1,35 @@
+function mostrarAlerta(){
+alert("se cargo la imagen correctamente");
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Obtener el botón para abrir el modal
+    var btnAbrir = document.getElementById('btnAbrirModal');
+
+    // Obtener el botón para cerrar el modal
+    var btnCerrar = document.getElementById('btnCerrarModal');
+
+    // Obtener el modal
+    var modal = document.getElementById('miModal');
+
+    // Función para abrir el modal
+    function abrirModal() {
+        modal.style.display = 'block'; // Mostrar el modal
+    }
+
+    // Función para cerrar el modal
+    function cerrarModal() {
+        modal.style.display = 'none'; // Ocultar el modal
+    }
+
+    // Asignar la función al evento onclick del botón de abrir modal
+    btnAbrir.onclick = abrirModal;
+
+    // Asignar la función al evento onclick del botón de cerrar modal
+    btnCerrar.onclick = cerrarModal;
+});
+
+ function mensaje(){
+   alert("Sus datos fueron enviados, gracias!");
+}
